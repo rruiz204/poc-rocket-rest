7a29163378c1ee3d6cb3740ff502a89a4709ce09
roney ruiz <roneyruizrojas@gmail.com>
2025-04-06 19:49:18 -0600

    feat: update diesel schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,2 +1,27 @@
 // @generated automatically by Diesel CLI.
 
+diesel::table! {
+    game (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+        players -> Int4,
+        release_date -> Date,
+        gender_id -> Int4,
+    }
+}
+
+diesel::table! {
+    gender (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+    }
+}
+
+diesel::joinable!(game -> gender (gender_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    game,
+    gender,
+);
