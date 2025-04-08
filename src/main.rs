ec884e63fdab39d2b6215c2596f87b5d4427e24c
roney ruiz <roneyruizrojas@gmail.com>
2025-04-07 22:41:41 -0600

    feat: adjust startup and main file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use startup::startup;
 
-pub mod routes;
-pub mod schema;
-pub mod models;
 pub mod startup;
-pub mod use_cases;
-pub mod controllers;
+
+pub mod domain;
+pub mod presentation;
+pub mod infrastructure;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
