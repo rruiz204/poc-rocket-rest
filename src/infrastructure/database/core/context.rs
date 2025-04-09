3e2e554d6240f59fb6669c84ac67214fb502ef9e
roney ruiz <roneyruizrojas@gmail.com>
2025-04-08 20:37:03 -0600

    feat: functional version of r2d2

--- src/infrastructure/database/core/context.rs
+++ src/infrastructure/database/core/context.rs
@@ -1,3 +1,16 @@
 use std::env;
 use dotenvy::dotenv;
 use diesel::pg::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
+
+type PgConn = ConnectionManager<PgConnection>;
+type PgPool = Pool<PgConn>;
+
+pub fn context() -> PgPool {
+  dotenv().ok();
+  let db_uri: String = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+  let manager: PgConn = ConnectionManager::<PgConnection>::new(db_uri);
+
+  Pool::builder().build(manager).expect("Failed to create database connection pool")
+}
