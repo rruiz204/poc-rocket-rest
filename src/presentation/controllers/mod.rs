7c0a28645f7883a5ab6b69a37b12acd23cf48366
roney ruiz <roneyruizrojas@gmail.com>
2025-04-16 22:01:03 -0600

    refactor: convert game controller into router

--- src/presentation/controllers/mod.rs
+++ src/presentation/controllers/mod.rs
@@ -1,1 +1,11 @@
+use rocket::Route;
+
 pub mod game_controller;
+
+pub fn get_routes() -> Vec<Route> {
+  let mut routes: Vec<Route> = Vec::new();
+
+  routes.extend(game_controller::GameController::get_routes());
+
+  routes
+}
