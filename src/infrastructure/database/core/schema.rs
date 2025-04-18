8698122761bbcca22270c42ed6eeee88fb62349f
roney ruiz <roneyruizrojas@gmail.com>
2025-04-17 21:33:54 -0600

    refactor: rename 'gender' to 'game gender' entity

--- src/infrastructure/database/core/schema.rs
+++ src/infrastructure/database/core/schema.rs
@@ -12,16 +12,16 @@ diesel::table! {
 }
 
 diesel::table! {
-    gender (id) {
+    gamegender (id) {
         id -> Int4,
         #[max_length = 255]
         name -> Varchar,
     }
 }
 
-diesel::joinable!(game -> gender (gender_id));
+diesel::joinable!(game -> gamegender (gender_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     game,
-    gender,
+    gamegender,
 );
