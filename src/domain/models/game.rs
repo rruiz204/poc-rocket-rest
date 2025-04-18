8945224e51b9d2f379b114056807f91ece24ce65
roney ruiz <roneyruizrojas@gmail.com>
2025-04-17 21:43:58 -0600

    chore: update models to changes

--- src/domain/models/game.rs
+++ src/domain/models/game.rs
@@ -2,16 +2,16 @@ use diesel::prelude::{Associations, Identifiable, Queryable};
 use serde::{Deserialize, Serialize};
 
 use crate::infrastructure::database::core::schema::game;
-use crate::domain::models::gender;
+use crate::domain::models::game_gender;
 use chrono::NaiveDate;
 
 #[derive(Queryable, Identifiable, Associations, Serialize, Deserialize, Debug)]
-#[diesel(belongs_to(gender::Gender))]
+#[diesel(belongs_to(game_gender::GameGender))]
 #[diesel(table_name = game)]
 pub struct Game {
   pub id: i32,
   pub name: String,
   pub players: i32,
   pub release_date: NaiveDate,
-  pub gender_id: i32,
+  pub game_gender_id: i32,
 }
--- src/domain/models/game_gender.rs
+++ src/domain/models/game_gender.rs
@@ -1,11 +1,11 @@
 use diesel::prelude::{Identifiable, Queryable};
 use serde::{Deserialize, Serialize};
 
-use crate::infrastructure::database::core::schema::gender;
+use crate::infrastructure::database::core::schema::gamegender;
 
 #[derive(Queryable, Identifiable, Serialize, Deserialize, Debug)]
-#[diesel(table_name = gender)]
-pub struct Gender {
+#[diesel(table_name = gamegender)]
+pub struct GameGender {
   pub id: i32,
   pub name: String,
 }
